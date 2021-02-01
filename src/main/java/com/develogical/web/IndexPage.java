94faef37d2f32bcb42e5d1118740c26b49d34b7c
Wenqiiii <wq16190747@163.com>
2021-02-01 16:04:24 +0000

    Change in HTML

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,8 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>Welcome!</h1>" +
+                "<h1>Welcome!</h1>" + 
+                "<h2>Automated deployment succeed!</h2>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
